66d32673dc95ea4590a3feb5683bf7c035a362ac
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2021-03-20 18:12:16 +0100

    Update stock-exchange.gateway.ts

--- src/api/gateways/stock-exchange.gateway.ts
+++ src/api/gateways/stock-exchange.gateway.ts
@@ -1,21 +1,29 @@
 import {
-  MessageBody, OnGatewayConnection, OnGatewayDisconnect,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
-  WebSocketGateway, WebSocketServer,
+  WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class StockExchangeGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  allStocks: string[] = [];
+  newStockValue = '';
+  allStocks: string[] = ['BuzzCo', 'FunCorp', 'Happy Ltd'];
   @WebSocketServer() server;
   @SubscribeMessage('update')
-  handleStocksEvent(@MessageBody() data: string): string {
-    console.log(data);
-    return data + 'hell0';
+  handleStocksEvent(@MessageBody() updatedValue: string): string {
+    console.log(updatedValue);
+    this.newStockValue = updatedValue; // WAS A PUSH
+    this.server.emit('stockValue', updatedValue); // IMPORTANT NAME
+    return updatedValue + 'hell0';
   }
 
-  handleConnection(client: any, ...args: any[]): any {
+  handleConnection(client: Socket, ...args: any[]): any {
     console.log('Client Connect', client.id);
+    client.emit('allStocks');
   }
 
   handleDisconnect(client: any): any {
