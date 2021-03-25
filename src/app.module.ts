58bd705e1536e9cbd2ec140a22a86934a0817cb3
SnowBoy73 <56825266+SnowBoy73@users.noreply.github.com>
2021-03-25 17:22:53 +0100

    docker running. Using port 3001 not 5000 (CORS issue)
    HOST localhost

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StockExchangeModule } from './api/stock-exchange.module';
 import { StockExchangeService } from './core/services/stock-exchange.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [StockExchangeModule],
+  imports: [StockExchangeModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
